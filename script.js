b34e3944ca2cdab1b86582443ad7cfd3026557a4
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-01 12:48:20 +0100

    Ajout de commentaires

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){      // On selectionne le fichier avec "document, ici, ready est l'évènement", et ensuite il ya la fct. dans les parenthèses du ready.
 
-    $(".par1").hide();
+    $(".par1").hide();             // On selectionne le selector class .par1.
 });
