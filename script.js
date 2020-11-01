60a49f261f762e77124dd0ba8688bec18fdba3b0
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-01 13:04:17 +0100

    Créatino d'évènements, disp. p5 en click s/ p1

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){      // On selectionne le fichier avec "document, ici, ready est l'évènement", et ensuite il ya la fct. dans les parenthèses du ready.
+$(function(){      // On selectionne le fichier avec "document, ici, ready est l'évènement", et ensuite il ya la fct. dans les parenthèses du ready.
+                    //On peut retirer le "document.ready" car jQuery le permet comme raccourci.
+    $("#p1").click(function(){
+        $("#p5").hide();           // Création d'un évènement dans un évènement. Le p5 disparait quand on clique sur le p1.
 
-    $(".par1").hide();             // On selectionne le selector class .par1.
+    });             // On selectionne le selector class .par1.
 });
