cad54d7c491ed69b71eb7abf0be03274bae3c5eb
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-01 23:01:38 +0100

    Ajout d'effets et de callback

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-$(function(){      // On selectionne le fichier avec "document, ici, ready est l'évènement", et ensuite il ya la fct. dans les parenthèses du ready.
-                    //On peut retirer le "document.ready" car jQuery le permet comme raccourci.
-    $("#p1").mouseenter(function(){
-        $("#p5").hide();
+$(function(){
 
+    $("#p1").click(function(){
+        $("#p5").hide("1000", function(){     // Ajout d'effets aux Events. Ici on a usé d'un callback, c'est à dire insérer une event dans un autre event. Il s'enclenche dès la fin du premier event.
+            $("#p2").hide("1000", function(){
+                $('#p7').hide("2000");        // On peut mettre autant de callback que l'on souhaite.
+            });          
+        });
     });
-
-    $("#p1").mouseleave(function(){
-        $("#p5").show();
-
-    });
-
-    $("#p3").hover(function(){          //  hover est une fct. particulière au sens où on peut séparer les 2 fct. avec juste une virgule, sans répéter la synthaxe complète de deux fct comme avec le mouseenter/leave.
-        $("#p4").hide();
     
-    },
-    function(){
-        $("#p4").show();
-    
-    });
-
-    $("#p6").on("click",function(){         // Avec le 'on', on définie après le type d'évènement déclencheur de l'eve. suivant, ici hide.
-        $("#p7").hide();
-
-    });
-
-    $("#p8").on({
-        mouseenter: function(){         // On reproduit les mêmes opérations que précédemment mais en condensant l'écriture.
-            $("#p9").hide();
-        },
-        mouseleave: function(){
-            $("#p9").show();
-        },
-        click: function(){
-            $("#p2").hide();
-        }
-
-        
-    });
-
-
 });
