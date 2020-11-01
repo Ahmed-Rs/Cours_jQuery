90c6b052f5c1f86505b7b34f015a3bfc7770cf7a
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-01 13:35:53 +0100

    Ecriture condensée des Events

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 $(function(){      // On selectionne le fichier avec "document, ici, ready est l'évènement", et ensuite il ya la fct. dans les parenthèses du ready.
                     //On peut retirer le "document.ready" car jQuery le permet comme raccourci.
-    $("#p1").click(function(){
-        $("#p5").hide();           // Création d'un évènement dans un évènement. Le p5 disparait quand on clique sur le p1.
+    $("#p1").mouseenter(function(){
+        $("#p5").hide();
 
-    });             // On selectionne le selector class .par1.
+    });
+
+    $("#p1").mouseleave(function(){
+        $("#p5").show();
+
+    });
+
+    $("#p3").hover(function(){          //  hover est une fct. particulière au sens où on peut séparer les 2 fct. avec juste une virgule, sans répéter la synthaxe complète de deux fct comme avec le mouseenter/leave.
+        $("#p4").hide();
+    
+    },
+    function(){
+        $("#p4").show();
+    
+    });
+
+    $("#p6").on("click",function(){         // Avec le 'on', on définie après le type d'évènement déclencheur de l'eve. suivant, ici hide.
+        $("#p7").hide();
+
+    });
+
+    $("#p8").on({
+        mouseenter: function(){         // On reproduit les mêmes opérations que précédemment mais en condensant l'écriture.
+            $("#p9").hide();
+        },
+        mouseleave: function(){
+            $("#p9").show();
+        },
+        click: function(){
+            $("#p2").hide();
+        }
+
+        
+    });
+
+
 });
