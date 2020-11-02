d055c57bbee284474f0746f32b986de69ff4ca2e
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-02 22:45:15 +0100

    Modifier des informations existantes

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ $(function(){
 
         var myVar3 = $("div").attr("style");  // La fct. attr(); nous permet d'afficher la valeur de l'attribut d'un élément.
         console.log(myVar3);
+
+        $("#p1").text("Voici mon nouveau texte"); // La fct. text(); utilisée sans arguments affiche le contenu textuel du #p1, sans le html. Or, on peut modifier le contenu du #p1 en mettant en arg. le nouveau texte souhaité.
+        $("#p2").html("Voila mon <b>nouveau</b> texte"); // Ici on change le code html avec la fct. idoine.
+        $("#myInput").val("Nouvelle valeur");   //De même on change la valeur de l'Input grâce à la fct. val();
     });
 
 });
