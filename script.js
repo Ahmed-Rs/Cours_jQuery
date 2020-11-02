98c3abc0ca2fd0c6d5a53eeb1b69f0d2873a1991
Ahmed Rassoul <ahmedrassoulahmed@gmail.com>
2020-11-02 14:01:30 +0100

    Stop animations et enchaînement d'EFFECTS

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 $(function(){
 
-    $("button").click(function(){
-        $("div").animate({      // On utilise la fct. animat(); qui prend comme arguments, d'abord l'objet auquel on peut apporter plusieurs modif. , puis le temps d'execution, puis si on veut une troisième argument qui est un callback.
-            left: '500px',         // qui s'executera une fois que l'animation est terminée.
-            opacity: '0.3',
-            width: '+=150',
-            height: '+=150px'
-        },5000);
+    $("#start").click(function(){
+        $(".d1").animate({left:'600px'}, 3000); // Ici les animations se déroulent les unes à la suite des autres et non simultanément comme dans le précédent commit.
+        $(".d1").animate({opacity: 0.2}, 3000);
+        $(".d1").animate({width:'200px'}, 3000);
+        $(".d1").animate({height:'200px'}, 3000);
+
     });
+
+    $("#stop").click(function(){
+        $(".d1").stop(true, true);    // Sans le true, il ya false par default et le stop n'arrêtera que la première animation et pas les autres. Avec le 1er true, il stope la 1ère anim. là où on a appuyé ainsi que les anim. qui devaient suivre.
+    });                         // Avec le 2e true, il laisse la première anim. arriver à son terme mais arrête les suivantes.
+
+    $("#slide").click(function(){
+        $(".d2").slideUp(3000).slideDown(3000).fadeOut(3000).fadeIn(3000);
+    });         // Pour faire s'enchaîner des EFFECTS, on peut mettre sur la même ligne, séparés d'un point, les différentes fct. qui entrent en jeu.
 
 });
